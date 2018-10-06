c0b33968be9453a6c1fac33416bfe4e90eb87b28
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2018-10-06 19:16:18 -0300

    Alterações nas funções de inserção

--- codigo/insereArvoreb.c
+++ codigo/insereArvoreb.c
@@ -2,59 +2,60 @@
 
 /*Descrição: Divide o nó em dois quando ele está cheio*/
 Arvore* dividir_no (Arvore *x, int i, Arvore *y) {
-    printf("Entrei aqui dividir no!\n");
    //Cria um novo nó que receberá os maiores elemntos de y;
    Arvore *z = criar();
    z->folha = y->folha;
    z->n = T-1;
    //Faz com que Z receba as chaves maiores após a mediana de y;
    for (int j = 0; j < T-1; j++){
-        z->chaves[j] = y->chaves[j+T+1];
+        z->chaves[j] = y->chaves[j+T];
    }
-   //Verifica se y é uma folha e se for passa os filhos das chaves maiores para Z;
+   //Verifica se y é uma folha e se for, passa os filhos das chaves maiores para Z;
    if (!y->folha){
        for(int j = 0; j < T; j++){
-            z->filhos[j] = y->filhos[j+T+1];
+            z->filhos[j] = y->filhos[j+T];
        }
    }
    y->n = T-1;
    //Movendo os filhos para abrir espaço para o novo nó Z;
-   for (int j = x->n+1; j > i+1; j--){
-        x->filhos[j+1] = x->filhos[j];
+   for (int j = x->n; j > i; j--){
+        x->filhos[j] = x->filhos[j-1];
    }
-   x->filhos[i] = z;
+   x->filhos[i+1] = z;
    //movendo as chaves para colocar a mediana de Y em X;
    for (int j = x->n; j > i; j--){
-        x->chaves[j+1] = x->chaves[j];
+        x->chaves[j] = x->chaves[j-1];
    }
-   x->chaves[i] = y->chaves[T];
-   x->n = x->n+1;
+   x->chaves[i] = y->chaves[T-1]; //jogando a mediana para cima;
+   x->n = x->n+1; //acrescentando n;
    return x;
 }
 
 /*Descrição: ????*/
 Arvore* inserir_arvore_nao_cheia (Arvore *x, TIPO k) {
-    printf("Entrei aqui Nao cheia!\n");
    int i = x->n;
    if(x->folha){
+        //"liberando" espaço para o K entrar.
         while(i>0 && k < x->chaves[i-1]){
             x->chaves[i] = x->chaves[i-1];
             i=i-1;
         }
         x->chaves[i] = k;
         x->n++;
-        return x;
    }
    else{
+        //encontrando o filho aonde o K deve entrar.
         while(i>0 && k < x->chaves[i-1]){
-            i = i-1;
+            i--;
         }
         //i = i+1;
-        if(x->filhos[i]->n = (2*T)-1){
+        if(x->filhos[i]->n == (2*T)-1){  //Verificar se o filho está cheio.
+            //imprimir(x,0);
             dividir_no(x,i,x->filhos[i]);
         }
         inserir_arvore_nao_cheia (x->filhos[i], k);
    }
+   return x;
 }
 
 /*Função para inserir uma chave em uma árvore B:*/
@@ -72,7 +73,5 @@ Arvore *inserir (Arvore *raiz, TIPO chave) {
       return inserir_arvore_nao_cheia (r, chave);
    }
 }
-
-
 
 
--- codigo/mainArvoreb.c
+++ codigo/mainArvoreb.c
@@ -13,26 +13,27 @@ int main () {
    a = inserir (a, 'K');
    a = inserir (a, 'C');
    a = inserir (a, 'L');
-   a = inserir (a, 'H');
-   a = inserir (a, 'T');
-   a = inserir (a, 'V');
-   a = inserir (a, 'W');
-   a = inserir (a, 'M');
-   a = inserir (a, 'R');
-   a = inserir (a, 'N');
-   a = inserir (a, 'P');
-   a = inserir (a, 'A');
-   a = inserir (a, 'B');
-   a = inserir (a, 'X');
-   a = inserir (a, 'Y');
-   a = inserir (a, 'D');
-   a = inserir (a, 'Z');
-   a = inserir (a, 'E');
+   //a = inserir (a, 'H');
+   //a = inserir (a, 'T');
+   //a = inserir (a, 'V');
+   //a = inserir (a, 'W');
+   //a = inserir (a, 'M');
+   //a = inserir (a, 'R');
+   //a = inserir (a, 'N');
+   //a = inserir (a, 'P');
+   //a = inserir (a, 'A');
+   //a = inserir (a, 'B');
+   //a = inserir (a, 'X');
+   //a = inserir (a, 'Y');
+   //a = inserir (a, 'D');
+   //a = inserir (a, 'Z');
+   //a = inserir (a, 'E');
 
+   /*printf("%c linha 1\n",a->chaves[0]);
+   printf("%c linha 1\n",a->chaves[1]);
+   printf("%c linha 1\n",a->chaves[2]);*/
+   //imprimir(a,0);
 
-   imprimir (a, 0);
-
-   //return 0;
-   int oi;
-   scanf("%d",&oi);
+   getchar();
+   return 0;
 }
