68bb34f09e9dff001f76132e4169035ce4d88213
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2019-04-08 22:23:06 -0300

    Alterações no P02. Parece estar concluído

--- p02/pingpong.c
+++ p02/pingpong.c
@@ -5,50 +5,69 @@
 #define STACKSIZE 32768		/* tamanho de pilha das threads */
 
 int taskid;
-ucontext_t contextMain;
+task_t contextMain, *current;
 
 // funções gerais ==============================================================
 
 void pingpong_init()
 {
-  /* desativa o buffer da saida padrao (stdout), usado pela função printf */
-  setvbuf (stdout, 0, _IONBF, 0);
-  taskid = 0;
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf (stdout, 0, _IONBF, 0);
+    taskid = 0;
+    contextMain.tid = taskid;
+    current = &contextMain;
 }
 
 // gerência de tarefas =========================================================
 
 int task_create (task_t *task, void (*start_func)(void *), void *arg)
 {
-  char *stack;
+    char *stack;
 
-  getcontext (&(task->context));
+    getcontext (&(task->context));
 
-  stack = malloc (STACKSIZE) ;
-  if (stack)
-  {
-     task->context.uc_stack.ss_sp = stack ;
-     task->context.uc_stack.ss_size = STACKSIZE;
-     task->context.uc_stack.ss_flags = 0;
-     task->context.uc_link = 0;
-     task->tid = taskid++;
-  }
-  makecontext(&(task->context),(void*)(*start_func),1,arg);
-  return task->tid;
+    stack = malloc (STACKSIZE) ;
+    if (stack)
+    {
+        task->context.uc_stack.ss_sp = stack ;
+        task->context.uc_stack.ss_size = STACKSIZE;
+        task->context.uc_stack.ss_flags = 0;
+        task->context.uc_link = 0;
+        task->tid = ++taskid;
+    }
+    else
+    {
+        perror ("Erro na criação da pilha: ");
+        exit (1);
+    }
+    makecontext(&(task->context),(void*)(*start_func),1,arg);
+    #ifdef DEBUG
+        printf("task_create: criou a tarefa: %d\n", task->tid);
+    #endif
+    return task->tid;
 }
 
 void task_exit (int exitCode)
 {
-
+    #ifdef DEBUG
+        printf("task_exit: tarefa %d sendo encerrada\n", current->tid);
+    #endif
+    task_switch(&contextMain);
 }
 
 int task_switch (task_t *task)
 {
-  swapcontext(&contextMain,&(task->context));
-  return 0;
+    #ifdef DEBUG
+        printf("task_switch: %d -> %d\n", current->tid, task->tid);
+    #endif
+    task_t *aux;
+    aux = current;
+    current = task;
+    swapcontext(&(aux->context),&(task->context));
+    return 0;
 }
 
 int task_id ()
 {
-
+    return current->tid;
 }
