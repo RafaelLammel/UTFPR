61abc638f9004b1891475f3651c62141950179ee
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2019-04-15 19:19:34 -0300

    Adiocinado função de print na 'queue' e inicio do p03

--- p03/pingpong.c
+++ p03/pingpong.c
@@ -2,20 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pingpong.h"
+#include "queue.h"
 #define STACKSIZE 32768		/* tamanho de pilha das threads */
+#define N 100
 
-int taskid;
-task_t contextMain, *current;
+typedef struct filatasks_t
+{
+   struct filatasks_t *prev;
+   struct filatasks_t *next;
+   task_t *t;
+} filatasks_t ;
+
+int taskid, userTasks;
+task_t contextMain, *current, *dispatcher;
+filatasks_t item;
+filatasks_t *filaProntas;
 
 // funções gerais ==============================================================
+
+task_t *scheduler()
+{
+    filatasks_t *aux;
+    aux = (filatasks_t*) queue_remove((queue_t**)&filaProntas,(queue_t*)filaProntas);
+    return aux->t;
+}
+
+void dispatcher_body ()
+{
+    task_t* next;
+    while ( userTasks > 0 )
+    {
+        next = scheduler();
+        if (next)
+        {
+            task_switch (next);
+        }
+        userTasks--;
+    }
+    task_exit(0);
+}
+
 
 void pingpong_init()
 {
     /* desativa o buffer da saida padrao (stdout), usado pela função printf */
     setvbuf (stdout, 0, _IONBF, 0);
     taskid = 0;
+    userTasks = 0;
     contextMain.tid = taskid;
     current = &contextMain;
+    task_create(dispatcher,(void *)dispatcher_body,NULL);
 }
 
 // gerência de tarefas =========================================================
@@ -41,25 +77,26 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg)
         exit (1);
     }
     makecontext(&(task->context),(void*)(*start_func),1,arg);
-    #ifdef DEBUG
-        printf("task_create: criou a tarefa: %d\n", task->tid);
-    #endif
+#ifdef DEBUG
+    printf("task_create: criou a tarefa: %d\n", task->tid);
+#endif
+    userTasks++;
     return task->tid;
 }
 
 void task_exit (int exitCode)
 {
-    #ifdef DEBUG
-        printf("task_exit: tarefa %d sendo encerrada\n", current->tid);
-    #endif
+#ifdef DEBUG
+    printf("task_exit: tarefa %d sendo encerrada\n", current->tid);
+#endif
     task_switch(&contextMain);
 }
 
 int task_switch (task_t *task)
 {
-    #ifdef DEBUG
-        printf("task_switch: %d -> %d\n", current->tid, task->tid);
-    #endif
+#ifdef DEBUG
+    printf("task_switch: %d -> %d\n", current->tid, task->tid);
+#endif
     task_t *aux;
     aux = current;
     current = task;
@@ -71,3 +108,16 @@ int task_id ()
 {
     return current->tid;
 }
+
+// operações de escalonamento ==================================================
+
+void task_yield ()
+{
+    item.next = NULL;
+    item.prev = NULL;
+    item.t = current;
+    queue_append((queue_t**) &filaProntas,(queue_t*) &item);
+    task_switch(dispatcher);
+}
+
+// =============================================================================
