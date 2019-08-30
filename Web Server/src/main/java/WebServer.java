9ab2db616f34c216d194f5d9e2075d812cb25052
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2019-08-29 23:50:21 -0300

    Projeto finalizado
    Adicionado a resposta do Servidor ao cliente e arquivos HTML, JPG e GIF para testes;

--- Web Server/src/main/java/WebServer.java
+++ Web Server/src/main/java/WebServer.java
@@ -70,10 +70,83 @@ final class HttpRequest implements Runnable{
             System.out.println(headerLine);
         }
 
+        //Extraindo o filename de requestLine
+        StringTokenizer tokens = new StringTokenizer(requestLine);
+        tokens.nextToken(); //Pula o método, que assumimos ser GET
+        String fileName = tokens.nextToken();
+
+        //Colocar um "." simbolizando que a requisição do arquivo esteja no diretório atual
+        fileName = "."+fileName;
+
+        //Abrindo o arquivo requisitado
+        FileInputStream fis = null;
+        boolean fileExists = true;
+        try{
+            fis = new FileInputStream(fileName);
+        }catch (FileNotFoundException e){
+            fileExists = false;
+        }
+
+        //Construindo a mensaegem de resposta
+        String statusLine = null;
+        String contentTypeLine = null;
+        String entityBody = null;
+        if(fileExists){
+            statusLine = "HTTP/1.0 200 OK" + CRLF;
+            contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+        }
+        else{
+            statusLine = "HTTP/1.0 404 Not Found" + CRLF;
+            contentTypeLine = "Content-type: text/html" + CRLF;
+            entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>"+"<BODY>Not Found</BODY></HTML>";
+        }
+
+        //Mandar a linha de Status
+        os.writeBytes(statusLine);
+
+        //Mandar a linha de contentType
+        os.writeBytes(contentTypeLine);
+
+        //Mandar a linha vazia para indicar o fim do Header
+        os.writeBytes(CRLF);
+
+        //Mandar o corpo entitidade
+        if(fileExists){
+            sendBytes(fis,os);
+            fis.close();
+        }
+        else{
+            os.writeBytes(entityBody);
+        }
+
         //Fechando streams e Socket
         os.close();
         br.close();
         socket.close();
+    }
+
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception{
+        //Construir um Buffer 1K para segurar os bytes a caminho do socket
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+
+        //Copiar o arquivo requisitado para o OutputStram do Socket
+        while((bytes = fis.read(buffer)) != -1){
+            os.write(buffer, 0, bytes);
+        }
+    }
+
+    private static String contentType(String fileName){
+        if(fileName.endsWith(".htm") || fileName.endsWith(".html")){
+            return "text/html";
+        }
+        if(fileName.endsWith(".gif")){
+            return "image/gif";
+        }
+        if(fileName.endsWith(".jpeg")){
+            return "image/jpeg";
+        }
+        return "application/octet-stream";
     }
 
 }
