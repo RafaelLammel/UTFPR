8d2d695ff9e6a08ec9404918af72715755467cac
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2019-10-08 22:26:10 -0300

    Colocando os dados do modelo em uma Matriz

--- servidor-ultrassom/src/main/java/edu/utfpr/servidor/ultrassom/process/ImageReconstruction.java
+++ servidor-ultrassom/src/main/java/edu/utfpr/servidor/ultrassom/process/ImageReconstruction.java
@@ -7,15 +7,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import no.uib.cipr.matrix.DenseMatrix;
 
 public class ImageReconstruction implements Runnable {
    
     private Imagem imagem;
     private Double[] g;
+    private DenseMatrix H;
     
     public ImageReconstruction(Imagem imagem, Double[] g){
         this.imagem = imagem;
         this.g = g;
+        this.H = new DenseMatrix(50816,3600);
     }
     
     @Override
@@ -29,9 +32,27 @@ public class ImageReconstruction implements Runnable {
     private void processCGNE(){
         try {
             BufferedReader br = new BufferedReader(new FileReader("./H-1.txt"));
+            StringBuffer s = new StringBuffer();
+            int i = 0, j = 0;
             while(br.ready()){
-                System.out.println(br.readLine());
+                s = new StringBuffer();
+                s.append(br.readLine());
+                StringBuffer num = new StringBuffer();
+                for(int k = 0; k < s.length(); k++){
+                    if(s.charAt(k) == ','){
+                        this.H.set(i, j, Double.parseDouble(num.toString()));
+                        num = new StringBuffer();
+                        j++;
+                    }
+                    else{
+                        num.append(s.charAt(k));
+                    }
+                }
+                this.H.set(i, j, Double.parseDouble(num.toString()));
+                i++;
+                j = 0;
             }
+            System.out.println("Items na matriz!");
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ImageReconstruction.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
