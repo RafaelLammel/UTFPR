69d20e615601d5e995b8538cf185395b4c843c90
tredeneo <tredeneo@hotmail.com>
2020-07-14 22:31:13 -0300

    implementado função de filtro separavel(sem reaproveitamento de somas) e com imagem integral

--- FiltroMedia/main.py
+++ FiltroMedia/main.py
@@ -7,11 +7,11 @@ CAMINHO_IMAGEM = 'Imagens/'
 INPUT_IMAGEM = 'b01 - Original.bmp'
 
 # Largura e Altura da janela (Escolher sempre tamanhos ímpares para ambos)
-LARGURA_JANELA = 3
-ALTURA_JANELA = 3
+LARGURA_JANELA = 21
+ALTURA_JANELA = 24
 
 
-ALGORITMO = 0
+ALGORITMO = 2
 '''
 Algoritmo a ser utilizado:
 0 - "Ingenuo" (WIP)
@@ -20,7 +20,7 @@ Algoritmo a ser utilizado:
 '''
 
 
-def ingenuo(img, w, h):
+def ingenuo(img, h, w):
     nova_img = np.copy(img)
 
     for i in range(h, len(img[0])-h):
@@ -35,12 +35,65 @@ def ingenuo(img, w, h):
     return nova_img
 
 
-def integral(img, w, h):
-    pass
+def integral(img, h, w):
+    # transformar em float
+    img = img.astype(np.float32) / 255
+    img_integral = np.copy(img)
+    # cria a imagem integral
+    for i in range(len(img[0])):
+        for j in range(len(img)):
+            if i == 0 and j == 0:
+                img_integral[i][j] = img[i][j]
+            elif i != 0 and j != 0:
+                img_integral[i][j] = (img[i][j]+img_integral[i][j-1] +
+                                      img_integral[i-1][j]-img_integral[i-1][j-1])
+            elif i == 0:
+                img_integral[i][j] = img[i][j]+img_integral[i][j-1]
+            elif j == 0:
+                img_integral[i][j] = img[i][j]+img_integral[i-1][j]
+    #cria imagem com a media
+    for i in range(len(img[0])):
+        for j in range(len(img)):
+            for k in range(h, 0, -1):
+                if i-k >= 0:
+                    break
+            for m in range(w, 0, -1):
+                if j-m >= 0:
+                    break
+            img[i][j] = pixel_integral(img_integral, i, j, k, m)
+
+    img = img * 255
+    img = img.astype(np.uint8)
+    return img
 
 
-def separaveis(img, w, h):
-    pass
+def pixel_integral(img_integral, i, j, h, w):
+    pixel = ((img_integral[i][j]-img_integral[i-h][j] -
+                  img_integral[i][j-w]+img_integral[i-h][j-w])/(w*h))
+    return pixel
+
+
+def separaveis(img, h, w):
+    nova_img = np.copy(img)
+    img_horizontal = np.copy(img)
+    for i in range(len(img[0])):
+        for j in range(w, len(img)-w):
+            soma = [0, 0, 0]
+            for l in range(int(j-w/2), int(j+w/2)):
+                soma[0] += img[i][l][0]
+                soma[1] += img[i][l][1]
+                soma[2] += img[i][l][2]
+            img_horizontal[i][j] = [s/w for s in soma]
+    for j in range(len(img)):
+        for i in range(h, len(img[0])-h):
+            soma = [0, 0, 0]
+            tamanho = [int(i-h/2), int(i+h/2)]
+            for k in range(tamanho[0], tamanho[1]):
+                soma[0] += img_horizontal[k][j][0]
+                soma[1] += img_horizontal[k][j][1]
+                soma[2] += img_horizontal[k][j][2]
+            nova_img[i][j] = [s/h for s in soma]
+    return nova_img
 
 
 def filtro_media(img, w, h):
