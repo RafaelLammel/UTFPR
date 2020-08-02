fab597a282c858e12f7c26de2a8de893759c7af2
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-08-02 16:37:25 -0300

    Arrumando bug da média de separaveis

--- FiltroMedia/main.py
+++ FiltroMedia/main.py
@@ -8,16 +8,15 @@ CAMINHO_IMAGEM = 'Imagens/'
 INPUT_IMAGEM = 'b01 - Original.bmp'
 
 # Largura e Altura da janela (Escolher sempre tamanhos ímpares para ambos)
-LARGURA_JANELA = 5
-ALTURA_JANELA = 5
+LARGURA_JANELA = 13
+ALTURA_JANELA = 13
 
 
-ALGORITMO = 0
+ALGORITMO = 1
 '''
 Algoritmo a ser utilizado:
 0 - "Ingenuo" (DONE - Ignorando margens)
-1 - Filtros Separáveis (BUG - Começaram a surgir alguns pixels com cores estranhas sem explicação 
-    em alguns pontos da imagem colorida, dentro da área filtrada)
+1 - Filtros Separáveis (DONE - Ignorando margens)
 2 - Imagens Integrais (DONE)
 '''
 
@@ -32,6 +31,44 @@ def ingenuo(img, w, h):
                 for i in range(x-math.floor(w/2), x+math.ceil(w/2)):
                     soma += img[i][j]
             nova_img[x][y] = soma / (w*h)
+
+    return nova_img
+
+
+def separaveis(img, w, h):
+    buffer = np.copy(img)
+    nova_img = np.copy(img)
+    
+    #Borra buffer
+    for y in range(len(img[0])):
+        soma = [0, 0, 0]
+        primeiro = True
+        for x in range(math.floor(w/2), len(img)-math.ceil(w/2)+1):
+            primeiro_pixel = x-math.floor(w/2)
+            ultimo_pixel = x+math.ceil(w/2)
+            if primeiro:
+                primeiro = False
+                for i in range(primeiro_pixel, ultimo_pixel):
+                    soma += img[i][y]
+            else:
+                soma = (soma - img[primeiro_pixel-1][y] + img[ultimo_pixel-1][y])
+            buffer[x][y] = soma / w
+
+    #Borra imagem final
+    for x in range(math.floor(w/2), len(img)-math.ceil(w/2)+1):
+        soma = [0, 0, 0]
+        primeiro = True
+        for y in range(math.floor(h/2), len(img[0])-math.ceil(h/2)+1):
+            primeiro_pixel = y-math.floor(h/2)
+            ultimo_pixel = y+math.ceil(h/2)
+            if primeiro:
+                primeiro = False
+                for i in range(primeiro_pixel, ultimo_pixel):
+                    soma += buffer[x][i]
+            else:
+                soma = (soma - buffer[x][primeiro_pixel-1] + buffer[x][ultimo_pixel-1])
+            nova_img[x][y] = soma / h
+
     return nova_img
 
 
@@ -71,44 +108,6 @@ def integral(img, w, h):
             nova_img[x][y] = (img_integral[topo][esquerda] - img_integral[topo][direita] + img_integral[inferior][direita] - img_integral[inferior][esquerda]) / ((altura if altura > 0 else 1) * (largura if largura > 0 else 1))
 
     return nova_img
-
-
-def separaveis(img, w, h):
-    buffer = np.copy(img)
-    nova_img = np.copy(img)
-    
-    #Borra buffer
-    for y in range(len(img[0])):
-        soma = [0, 0, 0]
-        primeiro = True
-        for x in range(int(w/2), len(img)-int(w/2)):
-            primeiro_pixel = int(x-w/2)
-            ultimo_pixel = int(x+w/2)
-            if primeiro:
-                primeiro = False
-                for i in range(primeiro_pixel, ultimo_pixel):
-                    soma += img[i][y]
-            else:
-                soma = (soma - img[primeiro_pixel][y] + img[ultimo_pixel][y])
-            buffer[x][y] = soma / w
-
-    #Borra imagem final
-    for x in range(int(w/2), len(img)-int(w/2)):
-        soma = [0, 0, 0]
-        primeiro = True
-        for y in range(int(h/2), len(img[0])-int(h/2)):
-            primeiro_pixel = int(y-h/2)
-            ultimo_pixel = int(y+h/2)
-            if primeiro:
-                primeiro = False
-                for i in range(primeiro_pixel, ultimo_pixel):
-                    soma += buffer[x][i]
-            else:
-                soma = (soma - buffer[x][primeiro_pixel] + buffer[x][ultimo_pixel])
-            nova_img[x][y] = soma / h
-
-    return nova_img
-
 
 def filtro_media(img, w, h):
     switch = {
