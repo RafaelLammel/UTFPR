86742995a14bc407de3ada52781945788ad458cd
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-08-05 17:01:33 -0300

    Primeiro commit do trabalho 01 (Sockets)

---
+++ CompartilhamentoNoticias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompartilhamentoNoticias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
