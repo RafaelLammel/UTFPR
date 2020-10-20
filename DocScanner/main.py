7263655095dd590e8fca087efb978123896e2b08
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-10-20 16:12:26 -0300

    Trocando valores fixos por constantes e adicionando binarização e fechamento antes de aplicar Canny

--- DocScanner/main.py
+++ DocScanner/main.py
@@ -6,23 +6,38 @@ import statistics
 PATH = "."
 PROCESS_PATH = "./Processadas"
 # Alterar para nome da imagem
-IMG = "img.jpeg"
+IMG = "img.jpg"
 
+CANNY_THRESHOLD1 = 200
+CANNY_THRESHOLD2 = 250
+
+HOUGH_RHO = 1
+HOUGH_THETA = np.pi/360
+HOUGH_THRESHOLD = 200
+
+FINAL_LENGTH = 674
+FINAL_HEIGHT = 467
+FINAL_FILE_TYPE = "jpg"
 
 def main():
     # Abrindo a imagem e mantendo uma cópia cinza
     img = cv2.imread(f"{PATH}/{IMG}")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"{PROCESS_PATH}/gray.jpeg", gray)
+    cv2.imwrite(f"{PROCESS_PATH}/gray.{FINAL_FILE_TYPE}", gray)
 
     # Borrando a imagem e aplicando Canny para um versão com bordas da imagem
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    cv2.imwrite(f"{PROCESS_PATH}/blur.jpeg", blur)
-    edges = cv2.Canny(blur, 50, 200)
-    cv2.imwrite(f"{PROCESS_PATH}/edges.jpeg", edges)
+    cv2.imwrite(f"{PROCESS_PATH}/blur.{FINAL_FILE_TYPE}", blur)
+    _, binary = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    cv2.imwrite(f"{PROCESS_PATH}/binay.{FINAL_FILE_TYPE}", binary)
+    kernel = np.ones((5,5), np.uint8)
+    closing = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel, iterations=5)
+    cv2.imwrite(f"{PROCESS_PATH}/closing.{FINAL_FILE_TYPE}", closing)
+    edges = cv2.Canny(closing, CANNY_THRESHOLD1, CANNY_THRESHOLD2)
+    cv2.imwrite(f"{PROCESS_PATH}/edges.{FINAL_FILE_TYPE}", edges)
 
     # Pegando as linhas da transformada de Hough
-    lines = cv2.HoughLines(edges, 1, np.pi/360, 150)
+    lines = cv2.HoughLines(edges, HOUGH_RHO, HOUGH_THETA, HOUGH_THRESHOLD)
     img_lines = np.copy(img)
     verticals = []
     horizontals = []
@@ -43,7 +58,7 @@ def main():
                 horizontals.append(rho / np.sin(theta))
             else:
                 verticals.append(rho / np.cos(theta))
-    cv2.imwrite(f"{PROCESS_PATH}/lines.jpeg", img_lines)
+    cv2.imwrite(f"{PROCESS_PATH}/lines.{FINAL_FILE_TYPE}", img_lines)
 
     # Deixando apenas 4 linhas
     horizontal_mean = np.mean(horizontals)
@@ -100,7 +115,7 @@ def main():
                 x2 = int(x0 - 2000*(-b))
                 y2 = int(y0 - 2000*(a))
                 cv2.line(paper_border, (x1, y1), (x2, y2), (0, 0, 255))
-    cv2.imwrite(f"{PROCESS_PATH}/PaperBorder.jpeg", paper_border)
+    cv2.imwrite(f"{PROCESS_PATH}/PaperBorder.{FINAL_FILE_TYPE}", paper_border)
     
     upper_left_point = tuple([int(i) for i in np.linalg.solve(np.array([[np.cos(upper["theta"]), np.sin(upper["theta"])], [np.cos(left["theta"]), np.sin(left["theta"])]]), np.array([upper["rho"], left["rho"]]))])
     upper_right_point = tuple([int(i) for i in np.linalg.solve(np.array([[np.cos(upper["theta"]), np.sin(upper["theta"])], [np.cos(right["theta"]), np.sin(right["theta"])]]), np.array([upper["rho"], right["rho"]]))])
@@ -112,16 +127,13 @@ def main():
     cv2.circle(points, upper_right_point, 30, (0,0,255), 10)
     cv2.circle(points, lower_left_point, 30, (0,0,255), 10)
     cv2.circle(points, lower_right_point, 30, (0,0,255), 10)
-    cv2.imwrite(f"{PROCESS_PATH}/Pontos.jpeg", points)
+    cv2.imwrite(f"{PROCESS_PATH}/Pontos.{FINAL_FILE_TYPE}", points)
 
     p1 = np.float32([list(upper_right_point), list(upper_left_point), list(lower_right_point), list(lower_left_point)])
-    p2 = np.float32([[0,0], [1080, 0], [0, 900], [1080, 900]])
+    p2 = np.float32([[0,0], [FINAL_LENGTH, 0], [0, FINAL_HEIGHT], [FINAL_LENGTH, FINAL_HEIGHT]])
     mat = cv2.getPerspectiveTransform(p1, p2)
-    out_img = cv2.warpPerspective(img, mat, (1080, 900))
-    cv2.imwrite(f"{PROCESS_PATH}/Final.jpeg", out_img)
-
-    _, binary = cv2.threshold(cv2.cvtColor(out_img, cv2.COLOR_BGR2GRAY), 150, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(f"{PROCESS_PATH}/Binarizada.jpeg", binary)
+    out_img = cv2.warpPerspective(img, mat, (FINAL_LENGTH, FINAL_HEIGHT))
+    cv2.imwrite(f"{PROCESS_PATH}/Final.{FINAL_FILE_TYPE}", out_img)
 
 
 if __name__ == "__main__":
