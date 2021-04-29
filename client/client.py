b9233e62e3c5af3dcbafdb27362e7cfc1419f2d0
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2021-04-29 18:39:48 -0300

    Adicionando Criptografia, Codificação de Linha (Manchester Diferencial) e mostrando gráfico no cliente
    Co-authored-by: Kelvin James

--- client/client.py
+++ client/client.py
@@ -1,27 +1,63 @@
 import socket
+import matplotlib.pyplot as plt
   
 PORT = 12345
 FORMAT = "ascii"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT = "!DISCONNECT"
+CHAVE_CRIPTO = 3
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-msg = input()
+# Digitar 
+print("Escreva sua mensagem: ", end="")
+msg = chr(19)
 
-# TODO: Criptografia
+# Criptografia
+cripto = ""
+for c in msg:
+    criptoChar = (ord(c) + CHAVE_CRIPTO) % 128
+    cripto += chr(criptoChar)
+print("Mensagem Criptografada: " + cripto)
 
 # Transforma em binário
-binary = ''.join(format(ord(i), '08b') for i in msg)
-print("binário:" + binary)
+binary = ''.join(format(ord(i), '08b') for i in cripto)
+print("Binario:" + binary)
 
-# TODO: Algorítmo de codificação de Linha (Manchester Diferencial)
+# Algorítmo de codificação de Linha (Manchester Diferencial)
+primeiraVez = True
+msg_codificada = ""
+anterior = ""
+for b in binary:
+    if primeiraVez:
+        if b == "0":
+            msg_codificada += "01"
+            anterior = "01"
+        else:
+            msg_codificada += "10"
+            anterior = "10"
+        primeiraVez = False
+    else:
+        if b == "1":
+            if anterior == "01":
+                msg_codificada += "10"
+                anterior = "10"
+            else:
+                msg_codificada += "01"
+                anterior = "01"
+        else:
+            msg_codificada += anterior
+print("Mensagem codificada em Manchester Diferencial: " + msg_codificada)
+
 # TODO: Apresentação do gráfico
-# TODO: Enviar para o servidor
+array = list(map(int, [c for c in msg_codificada]))
+plt.plot(array, drawstyle="steps-pre")
+plt.ylabel('Manchester Diferencial')
+plt.show()
 
-
-client.send(binary.encode(FORMAT))
+# Enviar para o servidor
+client.send(msg_codificada.encode(FORMAT))
 
 client.send(DISCONNECT.encode(FORMAT))
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'ascii'
 DISCONNECT = "!DISCONNECT"
+CHAVE_CRIPTO = 3
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -16,19 +17,46 @@ def handle_client(conn, addr):
         msg = conn.recv(1024)
         if msg.decode(FORMAT) == DISCONNECT:
             break
+        msg_codificada = msg.decode(FORMAT)
         # TODO: Apresentar o gráfico
-        # TODO: Aplicar o algoritmo de codificação de linha (Manchester Diferencial) modo inverso
+        # Aplicar o algoritmo de codificação de linha (Manchester Diferencial) modo inverso
+        primeiraVez = True
+        binary = ""
+        anterior = ""
+        for i in range(0, len(msg_codificada)-1, 2):
+            b = msg_codificada[i] + msg_codificada[i+1]
+            if primeiraVez:
+                if b == "01":
+                    binary += "0"
+                    anterior = "01"
+                else:
+                    binary += "1"
+                    anterior = "10"
+                primeiraVez = False
+            else:
+                if anterior == b:
+                    binary += "0"
+                    anterior = b
+                else:
+                    binary += "1"
+                    anterior = b
 
-        #Convertendo o binário  
-        binary = msg.decode(FORMAT)
+        #Convertendo o binário
+        print("Binario: " + binary)
         for i in range(0, len(binary), 8):
             temp_data = binary[i:i + 8]     
             decimal_data = int(temp_data, 2)
             str_data = str_data + chr(decimal_data) 
-        print("O valor binário após a conversão é:" + str_data)
         
-        # TODO: Algoritmo de criptografia inverso
-        # TODO: Mostrar a mensagem
+        # Algoritmo de criptografia inverso
+        print("Mensagem Criptografada: " + str_data)
+        descripto = ""
+        for c in str_data:
+            criptoChar = (ord(c) - CHAVE_CRIPTO) % 128
+            descripto += chr(criptoChar)
+        
+        # Mostrar a mensagem
+        print("Mensagem final: " + descripto)
     conn.close()
     print(f"[CONEXÃO DESCONECTADO] {addr} desconectado.")
 
