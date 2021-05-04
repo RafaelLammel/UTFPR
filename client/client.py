3bdc7c08ff77ecef2ed69ebf4d1f749da2ba8c4e
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2021-05-04 18:50:26 -0300

    Melhorias no código

--- client/client.py
+++ client/client.py
@@ -1,63 +1,77 @@
-import socket
+import socket, pickle
 import matplotlib.pyplot as plt
   
 PORT = 12345
-FORMAT = "ascii"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT = "!DISCONNECT"
-CHAVE_CRIPTO = 3
+CHAVE_CRIPTO = 5
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+def ceaserCipherEncoding(msg):
+    cripto = ""
+    for c in msg:
+        criptoChar = (ord(c) + CHAVE_CRIPTO) % 256
+        cripto += chr(criptoChar)
+    return cripto
 
-# Digitar 
-print("Escreva sua mensagem: ", end="")
-msg = chr(19)
-
-# Criptografia
-cripto = ""
-for c in msg:
-    criptoChar = (ord(c) + CHAVE_CRIPTO) % 128
-    cripto += chr(criptoChar)
-print("Mensagem Criptografada: " + cripto)
-
-# Transforma em binário
-binary = ''.join(format(ord(i), '08b') for i in cripto)
-print("Binario:" + binary)
-
-# Algorítmo de codificação de Linha (Manchester Diferencial)
-primeiraVez = True
-msg_codificada = ""
-anterior = ""
-for b in binary:
-    if primeiraVez:
-        if b == "0":
-            msg_codificada += "01"
-            anterior = "01"
-        else:
-            msg_codificada += "10"
-            anterior = "10"
-        primeiraVez = False
-    else:
-        if b == "1":
-            if anterior == "01":
-                msg_codificada += "10"
-                anterior = "10"
-            else:
+def difManchesterEncoding(bits):
+    primeiraVez = True
+    msg_codificada = ""
+    anterior = ""
+    for b in bits:
+        if primeiraVez:
+            if b == "0":
                 msg_codificada += "01"
                 anterior = "01"
+            else:
+                msg_codificada += "10"
+                anterior = "10"
+            primeiraVez = False
         else:
-            msg_codificada += anterior
-print("Mensagem codificada em Manchester Diferencial: " + msg_codificada)
+            if b == "1":
+                if anterior == "01":
+                    msg_codificada += "10"
+                    anterior = "10"
+                else:
+                    msg_codificada += "01"
+                    anterior = "01"
+            else:
+                msg_codificada += anterior
+    return msg_codificada
 
-# TODO: Apresentação do gráfico
-array = list(map(int, [c for c in msg_codificada]))
-plt.plot(array, drawstyle="steps-pre")
-plt.ylabel('Manchester Diferencial')
-plt.show()
+if __name__ == "__main__":
+    # Iniciando conexão
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
 
-# Enviar para o servidor
-client.send(msg_codificada.encode(FORMAT))
+    # Digitar 
+    print("Escreva sua mensagem: ", end="")
+    msg = input()
+    print()
 
-client.send(DISCONNECT.encode(FORMAT))
+    # Criptografia
+    cripto = ceaserCipherEncoding(msg)
+    print("Mensagem Criptografada: " + cripto)
+    print()
+
+    # Transforma em binário
+    binary = ''.join(format(ord(i), '08b') for i in cripto)
+    print("Binario: " + binary)
+    print()
+
+    # Algorítmo de codificação de Linha (Manchester Diferencial)
+    msg_codificada = difManchesterEncoding(binary)
+    print("Mensagem codificada em Manchester Diferencial: " + msg_codificada)
+    print()
+
+    # Apresentação do gráfico
+    bits = list(map(int, [c for c in msg_codificada]))
+    plt.plot(bits, drawstyle="steps-pre")
+    plt.ylabel('Manchester Diferencial')
+    plt.show()
+
+    # Enviar para o servidor
+    client.send(pickle.dumps(bits))
+
+    # Enviando mensagem para desconectar
+    client.send(pickle.dumps(DISCONNECT))
--- server/server.py
+++ server/server.py
@@ -1,70 +1,90 @@
-import socket
+import socket, pickle
+import matplotlib.pyplot as plt
 
 PORT = 12345
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-FORMAT = 'ascii'
 DISCONNECT = "!DISCONNECT"
-CHAVE_CRIPTO = 3
+CHAVE_CRIPTO = 5
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[NOVA CONEXÃO] {addr} conectado.")
-    str_data = ''
-    while True:
-        msg = conn.recv(1024)
-        if msg.decode(FORMAT) == DISCONNECT:
-            break
-        msg_codificada = msg.decode(FORMAT)
-        # TODO: Apresentar o gráfico
-        # Aplicar o algoritmo de codificação de linha (Manchester Diferencial) modo inverso
-        primeiraVez = True
-        binary = ""
-        anterior = ""
-        for i in range(0, len(msg_codificada)-1, 2):
-            b = msg_codificada[i] + msg_codificada[i+1]
-            if primeiraVez:
-                if b == "01":
-                    binary += "0"
-                    anterior = "01"
-                else:
-                    binary += "1"
-                    anterior = "10"
-                primeiraVez = False
-            else:
-                if anterior == b:
-                    binary += "0"
-                    anterior = b
-                else:
-                    binary += "1"
-                    anterior = b
+def ceaserCipherDecoding(msg):
+    descripto = ""
+    for c in msg:
+        criptoChar = (ord(c) - CHAVE_CRIPTO) % 256
+        descripto += chr(criptoChar)
+    return descripto
 
-        #Convertendo o binário
-        print("Binario: " + binary)
-        for i in range(0, len(binary), 8):
-            temp_data = binary[i:i + 8]     
-            decimal_data = int(temp_data, 2)
-            str_data = str_data + chr(decimal_data) 
+def difManchesterDecoding(binario_codificado):
+    primeiraVez = True
+    binario = ""
+    anterior = ""
+    for i in range(0, len(binario_codificado)-1, 2):
+        b = binario_codificado[i] + binario_codificado[i+1]
+        if primeiraVez:
+            if b == "01":
+                binario += "0"
+                anterior = "01"
+            else:
+                binario += "1"
+                anterior = "10"
+            primeiraVez = False
+        else:
+            if anterior == b:
+                binario += "0"
+                anterior = b
+            else:
+                binario += "1"
+                anterior = b
+    return binario
+
+def handleClient(conn, addr):
+    print(f"[NOVA CONEXÃO] {addr} conectado.")
+    while True:
+        # Recebendo dados
+        data = conn.recv(2048)
+        msg = ''.join([str(i) for i in pickle.loads(data)])
+        if msg == DISCONNECT:
+            break
+        msg_codificada = msg
+        
+        # Apresentar o gráfico
+        bits = list(map(int, [c for c in msg_codificada]))
+        plt.plot(bits, drawstyle="steps-pre")
+        plt.ylabel('Manchester Diferencial')
+        plt.show()
+
+        # Aplicar o algoritmo de codificação de linha (Manchester Diferencial) modo inverso
+        print()
+        print("Mensagem codificada em Manchester Diferencial: " + msg_codificada)
+        binario = difManchesterDecoding(msg_codificada)
+
+        # Convertendo o binário
+        print()
+        print("Binario: " + binario)
+        msg_cripto = ""
+        for i in range(0, len(binario), 8):
+            temp_msg = binario[i:i + 8]     
+            decimal_msg = int(temp_msg, 2)
+            msg_cripto =  msg_cripto + chr(decimal_msg) 
         
         # Algoritmo de criptografia inverso
-        print("Mensagem Criptografada: " + str_data)
-        descripto = ""
-        for c in str_data:
-            criptoChar = (ord(c) - CHAVE_CRIPTO) % 128
-            descripto += chr(criptoChar)
+        print()
+        print("Mensagem Criptografada: " +  msg_cripto)
+        descripto = ceaserCipherDecoding(msg_cripto)
         
         # Mostrar a mensagem
+        print()
         print("Mensagem final: " + descripto)
+        print()
     conn.close()
     print(f"[CONEXÃO DESCONECTADO] {addr} desconectado.")
 
-def start():
+if __name__ == "__main__":
+    print("[INICIANDO] Servidor iniciando...")
     server.listen()
     print(f"[ESCUTANDO] Servidor ouvindo em {SERVER}:{PORT}")
     conn, addr = server.accept()
     handle_client(conn, addr)
-
-print("[INICIANDO] Servidor iniciando...")
-start()
