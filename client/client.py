876f2429cc014376fff6629c228037b31538a988
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2021-05-08 17:25:35 -0300

    Melhorias no GUI do projeto (cliente e servidor)

--- client/client.py
+++ client/client.py
@@ -1,7 +1,9 @@
 import socket, pickle
+import sys
 import tkinter as tk
 from tkinter.constants import END
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
   
 PORT = 12345
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -10,28 +12,58 @@ DISCONNECT = "!DISCONNECT"
 CHAVE_CRIPTO = 5
 
 root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+graphFrame = tk.Frame(root)
+graphFrame.pack()
 
-msgEntry = tk.Entry(root, width=80)
-criptoEntry = tk.Entry(root, width=80)
-binarioEntry = tk.Entry(root, width=80)
-manchesterEntry = tk.Entry(root, width=80)
+msgEntry = tk.Entry(frame, width=80)
+criptoEntry = tk.Entry(frame, width=80)
+binarioEntry = tk.Entry(frame, width=80)
+manchesterEntry = tk.Entry(frame, width=80)
+
+# Iniciando conexão
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
 def gui():
 
-    tk.Label(root, text="Digite a mensagem: ").grid(row=0)
-    tk.Label(root, text="Mensagem Criptografada: ").grid(row=1)
-    tk.Label(root, text="Mensagem em Binário: ").grid(row=2)
-    tk.Label(root, text="Mensagem aplicado código manchester: ").grid(row=3)
+    tk.Label(frame, text="Digite a mensagem: ").grid(row=0)
+    tk.Label(frame, text="Mensagem Criptografada: ").grid(row=1)
+    tk.Label(frame, text="Mensagem em Binário: ").grid(row=2)
+    tk.Label(frame, text="Mensagem aplicado código manchester: ").grid(row=3)
 
     msgEntry.grid(row=0, column=1)
     criptoEntry.grid(row=1, column=1)
     binarioEntry.grid(row=2, column=1)
     manchesterEntry.grid(row=3, column=1)
 
-    button = tk.Button(root, text="Enviar", command=start)
+    button = tk.Button(frame, text="Enviar", command=start)
     button.grid(row=4)
 
+    disconnectButton = tk.Button(frame, text="Desconectar", command=disconnect)
+    disconnectButton.grid(row=4, column=1)
+
     root.mainloop()
+
+def drawGraph(bits, binario):
+    fig = plt.figure(figsize=(10, 3), dpi=100)
+    for i in range(0, len(bits)+1, 2):
+        plt.axvline(i, color=".5", linewidth=2)
+    plt.axhline(2.5, color='.5', linewidth=2)
+    plt.plot([b+2 for b in bits], color="r", linewidth=2, drawstyle="steps-pre")
+    plt.ylim([-1,6])
+    step = 0.9
+    for tbit, bit in enumerate(list(map(int, [c for c in binario]))):
+        plt.text(tbit + step, 3.5, str(bit))
+        step += 1
+    plt.gca().axis('off')
+    canvas = FigureCanvasTkAgg(fig, master=graphFrame)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=5, column=0)
+    toolbarFrame = tk.Frame(master=graphFrame)
+    toolbarFrame.grid(row=6,column=0)
+    NavigationToolbar2Tk(canvas, toolbarFrame)
 
 def ceaserCipherEncoding(msg):
     cripto = ""
@@ -65,11 +97,13 @@ def difManchesterEncoding(bits):
                 msg_codificada += anterior
     return msg_codificada
 
+def disconnect():
+    client.send(pickle.dumps(DISCONNECT))
+    root.destroy()
+    sys.exit()
+
 def start():
     msg = msgEntry.get()
-    # Iniciando conexão
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
 
     # Criptografia
     cripto = ceaserCipherEncoding(msg)
@@ -88,22 +122,9 @@ def start():
 
     # Apresentação do gráfico
     bits = list(map(int, [c for c in msg_codificada]))
-    for i in range(0, len(bits)+1, 2):
-        plt.axvline(i, color=".5", linewidth=2)
-    plt.axhline(2.5, color='.5', linewidth=2)
-    plt.plot([b+2 for b in bits], color="r", linewidth=2, drawstyle="steps-pre")
-    plt.ylim([-1,6])
-    step = 0.9
-    for tbit, bit in enumerate(list(map(int, [c for c in binary]))):
-        plt.text(tbit + step, 3.5, str(bit))
-        step += 1
-    plt.gca().axis('off')
-    plt.show()
+    drawGraph(bits, binary)
 
     # Enviar para o servidor
     client.send(pickle.dumps(bits))
-
-    # Enviando mensagem para desconectar
-    client.send(pickle.dumps(DISCONNECT))
 
 gui()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,11 @@
-import socket, pickle
+import socket, pickle, threading
+import sys
+import tkinter as tk
+from tkinter.constants import END
+import matplotlib
+matplotlib.use("agg")
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 PORT = 12345
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -9,6 +15,43 @@ CHAVE_CRIPTO = 5
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+def close():
+    root.destroy()
+    sys.exit()
+
+def gui():
+
+    tk.Label(frame, text="Mensagem aplicado código manchester: ").grid(row=0)
+    tk.Label(frame, text="Mensagem em Binário: ").grid(row=1)
+    tk.Label(frame, text="Mensagem Criptografada: ").grid(row=2)
+    tk.Label(frame, text="Mensagem final: ").grid(row=3)
+
+    manchesterEntry.grid(row=0, column=1)
+    binarioEntry.grid(row=1, column=1)
+    criptoEntry.grid(row=2, column=1)
+    msgEntry.grid(row=3, column=1)
+
+    root.mainloop()
+
+def drawGraph(bits, binario):
+    fig = plt.figure(figsize=(10, 3), dpi=100)
+    for i in range(0, len(bits)+1, 2):
+        plt.axvline(i, color=".5", linewidth=2)
+    plt.axhline(2.5, color='.5', linewidth=2)
+    plt.plot([b+2 for b in bits], color="r", linewidth=2, drawstyle="steps-pre")
+    plt.ylim([-1,6])
+    step = 0.9
+    for tbit, bit in enumerate(list(map(int, [c for c in binario]))):
+        plt.text(tbit + step, 3.5, str(bit))
+        step += 1
+    plt.gca().axis('off')
+    canvas = FigureCanvasTkAgg(fig, master=graphFrame)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=4, column=0, ipadx=40, ipady=20)
+    toolbarFrame = tk.Frame(master=graphFrame)
+    toolbarFrame.grid(row=5,column=0)
+    NavigationToolbar2Tk(canvas, toolbarFrame)
 
 def ceaserCipherDecoding(msg):
     descripto = ""
@@ -54,25 +97,15 @@ def handleClient(conn, addr):
         # Apresentar o gráfico
         bits = list(map(int, [c for c in msg_codificada]))
         binario = difManchesterDecoding(msg_codificada)
-        for i in range(0, len(bits)+1, 2):
-            plt.axvline(i, color=".5", linewidth=2)
-        plt.axhline(2.5, color='.5', linewidth=2)
-        plt.plot([b+2 for b in bits], color="r", linewidth=2, drawstyle="steps-pre")
-        plt.ylim([-1,6])
-        step = 0.9
-        for tbit, bit in enumerate(list(map(int, [c for c in binario]))):
-            plt.text(tbit + step, 3.5, str(bit))
-            step += 1
-        plt.gca().axis('off')
-        plt.show()
+        drawGraph(bits, binario)
         
         # Aplicar o algoritmo de codificação de linha (Manchester Diferencial) modo inverso
-        print()
-        print("Mensagem codificada em Manchester Diferencial: " + msg_codificada)
+        manchesterEntry.delete(0,END)
+        manchesterEntry.insert(0,msg_codificada)
 
         # Convertendo o binário
-        print()
-        print("Binario: " + binario)
+        binarioEntry.delete(0,END)
+        binarioEntry.insert(0,binario)
         msg_cripto = ""
         for i in range(0, len(binario), 8):
             temp_msg = binario[i:i + 8]     
@@ -80,20 +113,39 @@ def handleClient(conn, addr):
             msg_cripto =  msg_cripto + chr(decimal_msg) 
         
         # Algoritmo de criptografia inverso
-        print()
-        print("Mensagem Criptografada: " +  msg_cripto)
+        criptoEntry.delete(0,END)
+        criptoEntry.insert(0,msg_cripto)
         descripto = ceaserCipherDecoding(msg_cripto)
         
         # Mostrar a mensagem
-        print()
-        print("Mensagem final: " + descripto)
-        print()
+        msgEntry.delete(0,END)
+        msgEntry.insert(0,descripto)
     conn.close()
     print(f"[CONEXÃO DESCONECTADO] {addr} desconectado.")
+    close()
 
-if __name__ == "__main__":
+def serv():
     print("[INICIANDO] Servidor iniciando...")
     server.listen()
     print(f"[ESCUTANDO] Servidor ouvindo em {SERVER}:{PORT}")
     conn, addr = server.accept()
     handleClient(conn, addr)
+
+root = tk.Tk()
+root.protocol("WM_DELETE_WINDOW", close)
+
+frame = tk.Frame(root)
+frame.pack()
+graphFrame = tk.Frame(root)
+graphFrame.pack()
+
+threadServer = threading.Thread(target=serv)
+
+msgEntry = tk.Entry(frame, width=80)
+criptoEntry = tk.Entry(frame, width=80)
+binarioEntry = tk.Entry(frame, width=80)
+manchesterEntry = tk.Entry(frame, width=80)
+
+threadServer.daemon = True
+threadServer.start()
+gui()
