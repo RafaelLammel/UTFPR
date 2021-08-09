ddc591d17dbcba085ad10b58a518ec45cf0bee60
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2021-08-08 21:22:30 -0300

    Tela de loading ao entrar no app para verificar se usuário está ou não logado

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -3,7 +3,8 @@ import firebase from '../services/firebase';
 
 interface AuthData {
     signed: boolean,
-    user: object | null
+    user: object | null,
+    loading: boolean
 };
 
 const AuthContext = createContext<AuthData>({} as AuthData);
@@ -11,12 +12,15 @@ const AuthContext = createContext<AuthData>({} as AuthData);
 export const AuthProvider: React.FC = ({children}) => {
 
     const [user, setUser] = useState<object | null>(null);
+    const [loading, setLoading] = useState(true);
 
     function handleAuth(u: firebase.User | null) {
         if(u)
             setUser(u);
         else
             setUser(null);
+        if(loading)
+            setLoading(false);
     }
 
     useEffect(() => {
@@ -25,7 +29,7 @@ export const AuthProvider: React.FC = ({children}) => {
     }, []);
 
     return (
-        <AuthContext.Provider value={{signed: !!user, user}}>
+        <AuthContext.Provider value={{signed: !!user, user, loading}}>
             {children}
         </AuthContext.Provider>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
 import AuthRoutes from './authRoutes';
 import AppRoutes from './appRoutes';
 import AuthContext from '../contexts/auth';
 
 export default function Routes() {
     
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+
+    const theme = useTheme();
+
+    if(loading)
+        return(
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.background}}>
+                <ActivityIndicator size="large" color='#0000FF'/>
+            </View>
+        )
 
     return (
-        <>
-            {user ? <AppRoutes/> : <AuthRoutes/>}
-        </> 
+        user ? <AppRoutes/> : <AuthRoutes/>
     )
 }
